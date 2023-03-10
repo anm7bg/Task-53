9457be91dbae624b32340edc3acb2445933f6ad1
A. Maslarov <nsite2014@gmail.com>
2023-03-10 16:01:54 +0200

    Fix errors

--- src/js/app.js
+++ src/js/app.js
@@ -3,5 +3,17 @@ import "../scss/app.scss";
 window.addEventListener("DOMContentLoaded", () => {
   // This block will be executed once the page is loaded and ready
 
-console.log("hello")
+  console.log("hello")
+
+  const products = document.getElementsByClassName("product");
+  const data = document.getElementsByClassName("price");
+
+  console.log("hello");
+
+  for (let i = 0; i < products.length; i++) {
+    products[i].setAttribute("data-price", data[i].textContent)
+    console.log(products[i]);
+    console.log(data[i]);
+    
+  }
 });
